aec52490e02139c11fb24b705b1fd5bd64408bbe
mc2918 <tlsdud1375@gmail.com>
2021-02-27 23:52:48 +0900

    [Add] - form onchange 적용

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -2,17 +2,44 @@ import React, { useState } from 'react';
 import { Typography, Button, Form, Input } from 'antd';
 import { set } from 'mongoose';
  
-const { Title } = Typography;
+// const { Title } = Typography; 
 const { TextArea } = Input;
 
-function UploadProductPage(props) {
+// select
+const Continents = [
+  { key: 1, value: 'Africa' },
+  { key: 2, value: 'Europe' },
+  { key: 3, value: 'Asia' },
+  { key: 4, value: 'North America' },
+  { key: 5, value: 'South America' },
+  { key: 6, value: 'Australia' },
+  { key: 7, value: 'Antarctica' },
+]
+
+function UploadProductPage(props) { 
 
   const [ Title, setTitle] = useState('');
   const [ Description, setDescription ] = useState('');
+  const [ Price, setPrice ] = useState(0);
+  const [ Continent, setContinent ] = useState(1);
+  const [ Images, setImages ] = useState([]);
 
   const titleChangeHandler = (event) => {
     setTitle(event.currentTarget.value)
-    console.log(event.currentTarget.value);
+    console.log(event.currentTarget.value)
+  }
+
+  const descriptionChageHandler = (event) => {
+    setDescription(event.currentTarget.value)
+  }
+
+  const priceChangeHalder = (event) => {
+    setPrice(event.currentTarget.value)
+  }
+
+  const continentChangeHandelr = (event) => {
+    setContinent(event.currentTarget.value)
+    
   }
 
 
@@ -26,23 +53,27 @@ function UploadProductPage(props) {
         <br/>
         <br/>
         <label>이름</label>
-        <Input onChange={ titleChangeHandler }/>
+        <Input onChange={ titleChangeHandler } value={ Title }/>
         <br/>
         <br/>
         <label>설명</label>
-        <TextArea />
+        <TextArea onChange={ descriptionChageHandler } value={ Description } />
         <br/>
         <br/>
         <label>가격($)</label>
-        <Input />
+        <Input type="number" onChange={ priceChangeHalder } value={ Price } />
         <br/>
         <br/>
-        <select>
-          <option></option>
+        <select onChange={ continentChangeHandelr } value={ Continent }>
+          {
+            Continents.map(item => {
+              return <option key={item.key} value={item.key}>{item.value}</option>
+            })
+          }
         </select>
         <br/>
         <br/>
-        <Button>확인</Button>
+        <Button type="submit">Submit</Button>
       </Form>
     </div>
   );
